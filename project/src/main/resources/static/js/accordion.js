5a3c3934a2b8ff88b026b1eb08ed6d4019788484
aodlf452 <aodlf452@gmail.com>
2023-09-07 23:29:18 +0900

    강의페이지 아코디언 추가&메인->강의페이지 링크 테스트

---
+++ project/src/main/resources/static/js/accordion.js
@@ -0,0 +1,8 @@
+document
+  .querySelectorAll(".accordion .accordion_item > button")
+  .forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let accordion_item = e.currentTarget.parentNode;
+      accordion_item.classList.toggle("open");
+    });
+  });
