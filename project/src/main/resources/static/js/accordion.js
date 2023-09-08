45559be831579e40a57296c1798866b9dff8a83a
YoungJae Lee <aodlf452@gmail.com>
2023-09-08 14:53:59 +0900

    강의페이지 아코디언 수정&사이드 스티커 추가

--- project/src/main/resources/static/js/accordion.js
+++ project/src/main/resources/static/js/accordion.js
@@ -1,9 +1,24 @@
 //아코디언 클릭 이벤트 정의
-document
-  .querySelectorAll(".accordion .accordion_item > button")
-  .forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      let accordion_item = e.currentTarget.parentNode;
-      accordion_item.classList.toggle("open");
-    });
+// document
+//   .querySelectorAll(".accordion .accordion_item > button")
+//   .forEach((btn) => {
+//     btn.addEventListener("click", (e) => {
+//       let accordion_item = e.currentTarget.parentNode;
+//       accordion_item.classList.toggle("open");
+//     });
+//   });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
+}
