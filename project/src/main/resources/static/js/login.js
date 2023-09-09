b46c1708bfac70b28210ee9d9c2604978139c95e
YoungJae Lee <aodlf452@gmail.com>
2023-09-09 21:27:37 +0900

    .

--- project/src/main/resources/static/js/login.js
+++ project/src/main/resources/static/js/login.js
@@ -3,20 +3,29 @@ const password = document.getElementById("userPw");
 const login = document.getElementById("user_login");
 let errStack = 0;
 
-login.addEventListener("click", () => {
+function loginHandler() {
   if (id.value == "1971083") {
     if (password.value == "1111") {
-      alert("로그인 되었습니다!");
+      alert("로그인 성공");
       window.location.href = "new-main-demo.html";
     } else {
-      alert("아이디와 비밀번호를 다시 한 번 확인해주세요!");
+      alert("ID 또는 비밀번호 확인");
       errStack++;
     }
   } else {
-    alert("존재하지 않는 계정입니다.");
+    alert("존재하지 않는 계정");
   }
 
   if (errStack >= 5) {
     alert("비밀번호를 5회 이상 틀리셨습니다. 비밀번호 찾기를 권장드립니다.");
+  }
+}
+
+login.addEventListener("click", loginHandler);
+
+// Listen for Enter key press event
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    loginHandler();
   }
 });
