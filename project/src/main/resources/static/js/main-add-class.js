c616435aee4e62ba4e24f02e58f5222e120b1867
BuildingOwner <wodhks0911@gmail.com>
2023-09-15 23:43:00 +0900

    커뮤니티 새로고침 기능 추가

--- project/src/main/resources/static/js/main-add-class.js
+++ project/src/main/resources/static/js/main-add-class.js
@@ -1,24 +1,29 @@
 const MyClass = [
   {
-    className: '알고리즘',
-    dividion: '1',
-    prof: '상상부기'
+    imgsrc: "../../static/img/sangsangbugi.png",
+    className: "알고리즘",
+    dividion: "1",
+    prof: "상상부기",
   },
   {
-    className: '자료구조',
-    dividion: 'A',
-    prof: '상상부기2'
-  }
-]
+    imgsrc: "../../static/img/sangsangbugi.png",
+    className: "자료구조",
+    dividion: "A",
+    prof: "상상부기2",
+  },
+];
 
 const addClass = () => {
-  const classroom = document.querySelector('.classroom');
-  const tableTag = classroom.querySelector('table');
+  const classroom = document.querySelector(".classroom");
+  const tableTag = classroom.querySelector("table");
 
-  for(let i=0; i<MyClass.length; i++){
-    let newClass = document.createElement('tr');
-    newClass.innerHTML = `<td><img src="../../static/img/sangsangbugi.png" alt="" /></td><td>${MyClass[i].className}</td><td>${MyClass[i].dividion}</td><td>${MyClass[i].prof}</td>`;
+  for (let i = 0; i < MyClass.length; i++) {
+    const newClass = document.createElement("tr");
+    newClass.innerHTML = `
+    <td><img src="${MyClass[i].imgsrc}" alt="" /></td>
+    <td>${MyClass[i].className}</td><td>${MyClass[i].dividion}</td>
+    <td>${MyClass[i].prof}</td>`;
 
     tableTag.appendChild(newClass);
   }
-}
+};
---
+++ project/src/main/resources/static/js/main-add-community.js
@@ -0,0 +1,39 @@
+const RecoContents = [
+  {
+    title: "자소서 특강",
+    imgsrc: "../../static/img/sangsangbugi.png",
+    discription: "자기소개서 특강합니다!",
+    people: "11",
+  },
+  {
+    title: "직업윤리교육",
+    imgsrc: "../../static/img/sangsangbugi.png",
+    discription: "직업 의식 고취를 위한 특강",
+    people: "35",
+  },
+  {
+    title: "천원의 아침밥 시행안내",
+    imgsrc: "../../static/img/sangsangbugi.png",
+    discription: "천원의 아침밥 시행안내",
+    people: "693",
+  },
+];
+
+const addCommunity = () => {
+  const recoList = document.querySelector(".reco-list");
+  recoList.innerHTML = "";
+
+  for (let i = 0; i < RecoContents.length; i++) {
+    const newCommunity = document.createElement("div");
+    newCommunity.className = "reco-contents";
+    newCommunity.innerHTML = `
+      <div class="img-center">
+        <img src="${RecoContents[i].imgsrc}" alt="reco1Img" />
+      </div>
+      <h4 class="reco1-title">${RecoContents[i].title}</h4>
+      <span class="reco1-discription">${RecoContents[i].discription}</span>
+      <span class="reco1-people">${RecoContents[i].people}명 참여중</span>`;
+
+    recoList.appendChild(newCommunity);
+  }
+};
