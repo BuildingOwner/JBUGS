7918f9134cd80cf9bad13b38d4bee49931049a06
BuildingOwner <wodhks0911@gmail.com>
2023-09-16 01:11:33 +0900

    스프링 path 방식으로 변환

--- project/src/main/java/com/jbugs/project/controller/MainController.java
+++ project/src/main/java/com/jbugs/project/controller/MainController.java
@@ -13,4 +13,22 @@ public class MainController {
         log.info("main controller");
         return "html/new-main-demo";
     }
+
+    @RequestMapping("/new-lib.html")
+    public String library(){
+        log.info("main controller go new-lib");
+        return "html/new-lib";
+    }
+
+    @RequestMapping("/new-course-page.html")
+    public String course_page(){
+        log.info("main controller go course-page");
+        return "html/new-course-page";
+    }
+
+    @RequestMapping("/quiz.html")
+    public String quiz(){
+        log.info("main controller go quiz");
+        return "html/quiz";
+    }
 }
