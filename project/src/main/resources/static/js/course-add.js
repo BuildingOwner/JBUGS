1073d0a84047a18f3313fc75c1723cbca803c3c6
aodlf452 <aodlf452@gmail.com>
2023-09-19 08:51:56 +0900

    course-page css&js 수정 및 추가

--- project/src/main/resources/static/js/course-add.js
+++ project/src/main/resources/static/js/course-add.js
@@ -6,7 +6,7 @@ const MyContent = [
         videoname: "1주차 강의 1",
       },
       {
-        videosrc: "비디오경로",
+        videosrc: "/video/video2.mp4",
         videoname: "1주차 강의 2",
       },
       {
@@ -112,21 +112,41 @@ const addContent = () => {
       const newVideo = document.createElement("div");
       newVideo.classList.add("content-progress");
 
-      // Create video element
       const videoDiv = document.createElement("div");
-      videoDiv.classList.add("videoInfo")
-      videoDiv.innerHTML=`
-      <span>${MyContent[i].videos[j].videoname}</span>
-      `;
+      videoDiv.classList.add("videoInfo");
+
       const videoElement = document.createElement("video");
       videoElement.classList.add("class-content");
       videoElement.controls = true;
-      videoElement.width = 300;
+      videoElement.width = 600;
       videoElement.innerHTML = `
         <source src="${MyContent[i].videos[j].videosrc}" />   
       `;
-      videoDiv.appendChild(videoElement);
-      // Create progress bar
+      // 이전에 저장된 재생 시간 가져오기
+      const savedTime = localStorage.getItem(`videoTime-${i}-${j}`);
+
+      // 재생 시간 설정
+      if (savedTime) {
+        videoElement.currentTime = parseFloat(savedTime);
+      }
+
+      // 동영상 재생 시간 업데이트 및 저장
+      videoElement.addEventListener("timeupdate", function () {
+        const currentTime = videoElement.currentTime;
+        localStorage.setItem(`videoTime-${i}-${j}`, currentTime);
+      });
+
+      // 페이지 unload 시에 재생 시간 저장
+      window.addEventListener("beforeunload", function () {
+        const currentTime = videoElement.currentTime;
+        localStorage.setItem(`videoTime-${i}-${j}`, currentTime);
+      });
+
+      // Span 태그 추가 (videoInfo 내부)
+      const spanElement = document.createElement("span");
+      spanElement.textContent = MyContent[i].videos[j].videoname;
+
+      // Progress Bar
       const progressBar = document.createElement("div");
       progressBar.classList.add("bar-holder");
       const progressInner = document.createElement("div");
@@ -134,13 +154,18 @@ const addContent = () => {
       progressInner.textContent = "00%";
       progressBar.appendChild(progressInner);
 
-      // Append video and progress bar to the newVideo container
+      // Wrapping video title and progress bar in a div
+      const videoTitleAndProgressBar = document.createElement("div");
+      videoTitleAndProgressBar.classList.add("TitleAndProgressBar");
+      videoTitleAndProgressBar.appendChild(spanElement);
+      videoTitleAndProgressBar.appendChild(progressBar);
+
+      videoDiv.appendChild(videoElement);
+      videoDiv.appendChild(videoTitleAndProgressBar);
 
       newVideo.appendChild(videoDiv);
-      newVideo.appendChild(progressBar);
       panel.appendChild(newVideo);
 
-      // Add a timeupdate event listener to update the progress bar
       videoElement.addEventListener("timeupdate", () => {
         const currentTime = videoElement.currentTime;
         const duration = videoElement.duration;
@@ -163,5 +188,4 @@ const addContent = () => {
   }
 }
 
-// 호출하여 콘텐츠 추가
 addContent();
