19e1d29cede75f8793da9c9a9b5f50597c26728f
jangjuya <109902601+jangjuya@users.noreply.github.com>
2023-09-20 14:19:00 +0900

    최재완은 보아라

--- project/src/main/resources/static/js/main-add-class.js
+++ project/src/main/resources/static/js/main-add-class.js
@@ -42,26 +42,67 @@
 //     prof: "상상부기2",
 //   },
 // ];
-//
-// const addClass = () => {
-//   const classroom = document.querySelector(".classroom");
-//   const tableTag = classroom.querySelector("table");
-//
-//   for (let i = 0; i < MyClass.length; i++) {
-//     const newClass = document.createElement("tr");
-//     newClass.innerHTML = `
-//     <td><img src="${MyClass[i].imgsrc}" alt="" /></td>
-//     <td>${MyClass[i].className}</td><td>${MyClass[i].division}</td>
-//     <td>${MyClass[i].prof}</td>`;
-//
-//     newClass.addEventListener("click", () => {
-//       window.location.href = "course";
-//     });
-//
-//     tableTag.appendChild(newClass);
-//   }
-//
-//   if(tableTag.childElementCount === 8){
-//     classroom.style.marginBottom = "57px"
-//   }
-// };
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '13.125.120.116:3306',
+    user: 'jbugs',
+    password: '1234',
+    database: 'webdb'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('MySQL 연결 오류: ' + err.stack);
+        return;
+    }
+    console.log('MySQL에 연결되었습니다.');
+});
+
+
+let MyClass
+
+const query = 'SELECT * FROM classes';
+
+connection.query(query, (err, results) => {
+    if (err) {
+        console.error('쿼리 실행 오류: ' + err.stack);
+        return;
+    }
+
+    // JSON 형태로 결과 데이터를 변환
+    MyClass = JSON.stringify(results);
+
+    // 이제 jsonData를 원하는 대로 처리할 수 있습니다.
+    console.log('JSON 데이터: ' + jsonData);
+
+    // MySQL 연결 종료
+    connection.end();
+});
+
+
+
+
+const addClass = () => {
+  const classroom = document.querySelector(".classroom");
+  const tableTag = classroom.querySelector("table");
+
+  for (let i = 0; i < MyClass.length; i++) {
+    const newClass = document.createElement("tr");
+    newClass.innerHTML = `
+    <td><img src="${MyClass[i].imgsrc}" alt="" /></td>
+    <td>${MyClass[i].className}</td><td>${MyClass[i].division}</td>
+    <td>${MyClass[i].prof}</td>`;
+
+    newClass.addEventListener("click", () => {
+      window.location.href = "course";
+    });
+
+    tableTag.appendChild(newClass);
+  }
+
+  if(tableTag.childElementCount === 8){
+    classroom.style.marginBottom = "57px"
+  }
+};
