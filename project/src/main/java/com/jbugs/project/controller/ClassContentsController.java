68e3759497e4468de1b24a28c281c7aa3db3bc79
jangjuya <109902601+jangjuya@users.noreply.github.com>
2023-09-24 21:04:59 +0900

    fileupload

--- project/src/main/java/com/jbugs/project/controller/ClassContentsController.java
+++ project/src/main/java/com/jbugs/project/controller/ClassContentsController.java
@@ -2,13 +2,14 @@ package com.jbugs.project.controller;
 
 import com.jbugs.project.domain.ClassContents;
 import com.jbugs.project.domain.Classes;
-import com.jbugs.project.domain.File;
 import com.jbugs.project.domain.Video;
 import com.jbugs.project.service.ClassContentsService;
 import com.jbugs.project.service.ClassService;
 import com.jbugs.project.service.FileService;
 import com.jbugs.project.service.VideoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -23,12 +26,17 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ClassContentsController {
 
     private final ClassContentsService classContentsService;
     private final ClassService classService;
     private final FileService fileService;
     private final VideoService videoService;
+
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @GetMapping("/developer")
     public String creatForm(Model model){
@@ -49,9 +57,19 @@ public class ClassContentsController {
                         @RequestParam("file") MultipartFile file,
                         @RequestParam("homeworkTitle")String homeworkTitle,
                         @RequestParam("homeworkDescription")String homeworkDescription,
-                        @RequestParam("homeworkDeadline") String homeworkDeadline){
+                        @RequestParam("homeworkDeadline") String homeworkDeadline) throws IOException {
+            String fullFilePath = fileDir + classes +"/"+ week +"/file/"+file.getOriginalFilename();
+            String fileName= file.getOriginalFilename();
+            log.info("파일 저장 fullFilePath={}", fullFilePath);
+            file.transferTo(new File(fullFilePath));
 
-        classContentsService.order(classes, videoFile, file, week, homeworkTitle, homeworkDescription, homeworkDeadline);
+            String fullVideoPath = fileDir + classes +"/"+ week +"/video/"+videoFile.getOriginalFilename();
+            String videoName = videoFile.getOriginalFilename();
+            log.info("파일 저장 fullVideoPath={}", fullVideoPath);
+            videoFile.transferTo(new File(fullVideoPath));
+
+
+        classContentsService.order(classes, videoFile, file, week, homeworkTitle, homeworkDescription, homeworkDeadline,fullFilePath, fullVideoPath, fileName, videoName);
         System.out.println(classes+ week  +  homeworkTitle + homeworkDescription + homeworkDeadline);
         System.out.println(videoFile);
         System.out.println(file);
